ff6d15a743101f63489fd68c661ee1f7c6d73d60
Vinoth Kumar V <vinoth_kumar_v@Vinoths-MacBook-Pro.local>
2023-08-07 23:39:46 +0530

    layout Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-h-full">
+      <header className='flex items-center justify-center text bg-blue-500 h-20 text-white font-black'>My Challenges List</header>
+      <div className='flex items-center justify-between flex-wrap m-5'>
+        <div className='w-80 h-60 mt-5 rounded border-solid border-2 border-indego-600'></div>
+        <div className='w-80 h-60 mt-5 rounded border-solid border-2 border-indego-600'></div>
+        <div className='w-80 h-60 mt-5 rounded border-solid border-2 border-indego-600'></div>
+        <div className='w-80 h-60 mt-5 rounded border-solid border-2 border-indego-600'></div>
+        <div className='w-80 h-60 mt-5 rounded border-solid border-2 border-indego-600'></div>
+        <div className='w-80 h-60 mt-5 rounded border-solid border-2 border-indego-600'></div>
+        <div className='w-80 h-60 mt-5 rounded border-solid border-2 border-indego-600'></div>
+      </div>
     </div>
   );
 }
